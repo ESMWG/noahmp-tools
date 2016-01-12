50783cb0c86a287991f9aab3e1f7b8a6fc857334
Hui Zheng <zhenghui@login2.stampede.tacc.utexas.edu>
2016-01-12 10:20:11 -0600

    weakref to ldasoutdiff.py

--- ldasoutdiff.py
+++ ldasoutdiff.py
@@ -8,62 +8,70 @@ import datetime
 import numpy as np
 import netCDF4 as nc
 import argparse
+import dateutil.parser
 
-def main(caseroot):
+def main(caseroot, endtime):
     # find all outputs
     files = sorted(glob.glob(os.path.join(caseroot,'*.LDASOUT_DOMAIN1')), reverse=True)
     for iflnm, flnm in enumerate(files[:-1]):
         tc = datetime.datetime.strptime(os.path.basename(flnm).split('.')[0], '%Y%m%d%H')
+        if (endtime is not None) and (tc > endtime):
+            continue
         tp = datetime.datetime.strptime(os.path.basename(files[iflnm+1]).split('.')[0], '%Y%m%d%H')
         dt = tc - tp
         print(flnm)
         if dt.total_seconds() != 3 * 3600:
             print('missing file for ', tc - datetime.timedelta(hours=3))
             exit(1)
-        with nc.Dataset(flnm, 'r+') as fc, \
-             nc.Dataset(files[iflnm+1], 'r') as fp:
-            rnugdp = fp.variables['UGDRNOFF'][:]
-            rnugdc = fc.variables['UGDRNOFF'][:]
-            fc.variables['UGDRNOFF'][:] = (rnugdc - rnugdp) / dt.total_seconds()
-            fc.variables['UGDRNOFF'].units = 'mm s-1'.encode('ascii')
-            fc.variables['UGDRNOFF'].description = 'underground runoff'.encode('ascii')
+        with nc.Dataset(files[iflnm+1], 'r', keepweakref=True) as f:
+            rnugdp = f.variables['UGDRNOFF'][:]
+            rnsfcp = f.variables['SFCRNOFF'][:]
+            snowp = f.variables['ACSNOW'][:]
+            snomp = f.variables['ACSNOM'][:]
+        with nc.Dataset(flnm, 'a', keepweakref=True) as f:
+            rnugdc = f.variables['UGDRNOFF'][:]
+            f.variables['UGDRNOFF'][:] = (rnugdc - rnugdp) / dt.total_seconds()
+            f.variables['UGDRNOFF'].units = 'mm s-1'.encode('ascii')
+            f.variables['UGDRNOFF'].description = 'underground runoff'.encode('ascii')
 
-            rnsfcp = fp.variables['SFCRNOFF'][:]
-            rnsfcc = fc.variables['SFCRNOFF'][:]
-            fc.variables['SFCRNOFF'][:] = (rnsfcc - rnsfcp) / dt.total_seconds()
-            fc.variables['SFCRNOFF'].units = 'mm s-1'.encode('ascii')
-            fc.variables['SFCRNOFF'].description = 'surface runoff'.encode('ascii')
+            rnsfcc = f.variables['SFCRNOFF'][:]
+            f.variables['SFCRNOFF'][:] = (rnsfcc - rnsfcp) / dt.total_seconds()
+            f.variables['SFCRNOFF'].units = 'mm s-1'.encode('ascii')
+            f.variables['SFCRNOFF'].description = 'surface runoff'.encode('ascii')
 
-            snowp = fp.variables['ACSNOW'][:]
-            snowc = fc.variables['ACSNOW'][:]
-            fc.variables['ACSNOW'][:] = (snowc - snowp) / dt.total_seconds()
-            fc.variables['ACSNOW'].units = 'mm s-1'.encode('ascii')
-            fc.variables['ACSNOW'].description = 'snow fall'.encode('ascii')
+            snowc = f.variables['ACSNOW'][:]
+            f.variables['ACSNOW'][:] = (snowc - snowp) / dt.total_seconds()
+            f.variables['ACSNOW'].units = 'mm s-1'.encode('ascii')
+            f.variables['ACSNOW'].description = 'snow fall'.encode('ascii')
 
-            snomp = fp.variables['ACSNOM'][:]
-            snomc = fc.variables['ACSNOM'][:]
-            fc.variables['ACSNOM'][:] = (snomc - snomp) / dt.total_seconds()
-            fc.variables['ACSNOM'].units = 'mm s-1'.encode('ascii')
-            fc.variables['ACSNOM'].description = 'melting water out of snow bottom'.encode('ascii')
+            snomc = f.variables['ACSNOM'][:]
+            f.variables['ACSNOM'][:] = (snomc - snomp) / dt.total_seconds()
+            f.variables['ACSNOM'].units = 'mm s-1'.encode('ascii')
+            f.variables['ACSNOM'].description = 'melting water out of snow bottom'.encode('ascii')
     print(files[-1])
-    with nc.Dataset(files[-1], 'r+') as fc:
-        fc.variables['UGDRNOFF'][:] = 0.0
-        fc.variables['UGDRNOFF'].units = 'mm s-1'.encode('ascii')
-        fc.variables['UGDRNOFF'].description = 'underground runoff'.encode('ascii')
-        fc.variables['SFCRNOFF'][:] = 0.0
-        fc.variables['SFCRNOFF'].units = 'mm s-1'.encode('ascii')
-        fc.variables['SFCRNOFF'].description = 'surface runoff'.encode('ascii')
-        fc.variables['ACSNOW'][:] = 0.0
-        fc.variables['ACSNOW'].units = 'mm s-1'.encode('ascii')
-        fc.variables['ACSNOW'].description = 'snow fall'.encode('ascii')
-        fc.variables['ACSNOM'][:] = 0.0
-        fc.variables['ACSNOM'].units = 'mm s-1'.encode('ascii')
-        fc.variables['ACSNOM'].description = 'melting water out of snow bottom'.encode('ascii')
+    with nc.Dataset(files[-1], 'a') as f:
+        f.variables['UGDRNOFF'][:] = 0.0
+        f.variables['UGDRNOFF'].units = 'mm s-1'.encode('ascii')
+        f.variables['UGDRNOFF'].description = 'underground runoff'.encode('ascii')
+        f.variables['SFCRNOFF'][:] = 0.0
+        f.variables['SFCRNOFF'].units = 'mm s-1'.encode('ascii')
+        f.variables['SFCRNOFF'].description = 'surface runoff'.encode('ascii')
+        f.variables['ACSNOW'][:] = 0.0
+        f.variables['ACSNOW'].units = 'mm s-1'.encode('ascii')
+        f.variables['ACSNOW'].description = 'snow fall'.encode('ascii')
+        f.variables['ACSNOM'][:] = 0.0
+        f.variables['ACSNOM'].units = 'mm s-1'.encode('ascii')
+        f.variables['ACSNOM'].description = 'melting water out of snow bottom'.encode('ascii')
 
     return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('caseroot')
+    parser.add_argument('-e', '--endtime')
     args = parser.parse_args()
-    main(args.caseroot)
+    if args.endtime is None:
+        endtime = None
+    else:
+        endtime = dateutil.parser.parse(args.endtime)
+    main(args.caseroot, endtime)
